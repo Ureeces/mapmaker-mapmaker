01eab9642b90c73ceb1efefd7e8be9520658c439
Ureeces <Ulysses.Reyes@codeimmersives.com>
2020-07-01 14:25:30 -0400

    Finished all functions

--- main.js
+++ main.js
@@ -1,6 +1,100 @@
 /******************
  * YOUR CODE HERE *
 ******************/
+const doubleAll = function(nums) {
+  const doubled = [];
+  for(let i = 0; i < nums.length; i++) {
+    doubled.push(nums[i] * 2);
+  }
+
+  return doubled;
+}
+
+const yelledGreetings = function(greetings) {
+  const yells = [];
+  for(let i = 0; i < greetings.length; i++) {
+    yells.push(greetings[i] + "!");
+  }
+
+  return yells;
+}
+
+const absoluteValues = function(nums) {
+  const absolutes = [];
+  for(let i = 0; i < nums.length; i++) {
+    absolutes.push(Math.abs(nums[i]));
+  }
+
+  return absolutes;
+}
+
+const upperCaseFirstLetters = function(words) {
+  const upperCased = [];
+  for(let i = 0; i < words.length; i++) {
+    let currWord = words[i];
+    
+    let formattedWord = currWord[0].toUpperCase() 
+    + currWord.slice(1).toLowerCase();
+    
+    upperCased.push(formattedWord);
+  }
+
+  return upperCased;
+}
+
+const changeToInitials = function(names) {
+  const initials = [];
+  for(let i = 0; i < names.length; i++) {
+    let currName = names[i];
+    
+    let firstInitial = currName[0].toUpperCase();
+    
+    let lastIndex = currName.indexOf(" ");
+    let lastInitial = currName[lastIndex + 1].toUpperCase();
+
+    initials.push(firstInitial + lastInitial);
+  }
+
+  return initials;
+}
+
+const doubleOdd = function(nums) {
+  const doubledOdds = [];
+
+  for(let i = 0; i < nums.length; i++) {
+    let currNum = nums[i];
+    let isOdd = Math.abs(Math.floor(currNum)) % 2 !== 0 ? true : false;
+    
+    if(isOdd) {
+      doubledOdds.push(currNum * 2);
+    }
+
+    else {
+      doubledOdds.push(currNum);
+    }
+  }
+
+  return doubledOdds;
+}
+
+const add1ToLeft = function(nums) {
+  const numsWith1 = [];
+
+  for(let i = 0; i < nums.length; i++) {
+    let currNum = "" + nums[i];
+    if(currNum[0] === "-") {
+      currNum = "-1" + currNum.slice(1);
+    }
+
+    else {
+      currNum = "1" + currNum;
+    }
+
+    numsWith1.push(Number(currNum));
+  }
+
+  return numsWith1;
+}
 
 
 
